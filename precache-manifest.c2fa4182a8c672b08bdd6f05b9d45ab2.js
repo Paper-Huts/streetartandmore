c84211a03e3619d28f66055bdc0fe07254169717
Edwin Agbenyega <eddybanks03@gmail.com>
2019-11-02 22:57:46 -0600

    Updates

---
+++ precache-manifest.c2fa4182a8c672b08bdd6f05b9d45ab2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b01d887d993f04a67cd2b2727515d7e3",
+    "url": "paper-huts.github.io/streetartandmore/index.html"
+  },
+  {
+    "revision": "77d40dcd81a563ab2c14",
+    "url": "paper-huts.github.io/streetartandmore/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "308ade5b25aa9fac33de",
+    "url": "paper-huts.github.io/streetartandmore/static/js/2.9c0ed561.chunk.js"
+  },
+  {
+    "revision": "77d40dcd81a563ab2c14",
+    "url": "paper-huts.github.io/streetartandmore/static/js/main.fa509819.chunk.js"
+  },
+  {
+    "revision": "061c1653f0856f892460",
+    "url": "paper-huts.github.io/streetartandmore/static/js/runtime-main.651d8f4a.js"
+  }
+]);
